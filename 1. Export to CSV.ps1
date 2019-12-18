6971a3d583cc0ee3447ccc8ee1761de4170d54a5
enablingpenguin <58984569+enablingpenguin@users.noreply.github.com>
2019-12-18 14:12:15 -0500

    Add files via upload

---
+++ 1. Export to CSV.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host -ForegroundColor Magenta "Exporting All Chrome OS Devices to C:\GAM\Scripts\Devices.csv"
+
+C:\GAM\gam.exe print cros serialnumber annotatedAssetId lastEnrollmentTime orgUnitPath | Out-File C:\GAM\Scripts\devices.csv -Encoding utf8
+
+Write-Host -ForegroundColor Magenta "Export Complete, Edit CSV File then run Update From CSV script"
+
+Write-Host -ForegroundColor Magenta "DO NOT CHANGE DEVICE ID"
+Write-Host -ForegroundColor Magenta "ONLY CHANGE ASSET ID"
+Write-Host -ForegroundColor Magenta "YOU CAN REMOVE UN-NEEDED ROWS"
+
+Write-Host -ForegroundColor Magenta "Press any key to exit..."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
---
+++ 2. Update From CSV.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host -ForegroundColor Magenta "WARNING: THIS SCRIPT WILL UPDATE ASSET IDs FROM C:\GAM\Scripts\devices.csv"
+
+Write-Host -ForegroundColor Magenta "ARE YOU SURE? Press any key to continue..."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+C:\GAM\gam.exe csv C:\GAM\Scripts\devices.csv gam update cros ~deviceId assetid ~annotatedAssetId ou """~orgUnitPath"""
+
+Write-Host -ForegroundColor Magenta "Update Complete, Press any key to exit..."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
